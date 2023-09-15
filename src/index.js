390b1e0b5ab2150bdff354a26c1510e16a5b6510
unknown <C:\The Bat!>
2023-09-15 15:52:37 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -2,23 +2,26 @@ import './pages/index.less';
 
 
 // Анимация счётчика цифр при загрузке страницы
-const numbers = document.querySelectorAll('.hero__number');
+window.onload = function () {
+    const numbers = document.querySelectorAll('.hero__number');
 
-numbers.forEach(number => {
-    let stop = parseInt(number.getAttribute('data-stop'));
-    let count = 0;
-    let increment = stop / 100;
+    numbers.forEach(number => {
+        let stop = parseInt(number.getAttribute('data-stop'));
+        let count = 0;
+        let increment = stop / 100;
 
-    let timer = setInterval(() => {
-        count += increment;
-        number.textContent = Math.floor(count);
+        let timer = setInterval(() => {
+            count += increment;
+            number.textContent = Math.floor(count);
 
-        if (count >= stop) {
-            clearInterval(timer);
-            number.textContent = stop;
-        }
-    }, 15);
-});
+            if (count >= stop) {
+                clearInterval(timer);
+                number.textContent = stop;
+            }
+        }, 15);
+    });
+}
+
 
 
 
